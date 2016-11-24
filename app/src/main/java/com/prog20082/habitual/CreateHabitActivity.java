78f3b51af619ed4fa567f3e17834ac02cc8150c6
cottageGrove <rafae.pooba@gmail.com>
2016-11-24 17:32:58 -0500

    Trying to do something with the createHabitActivity

--- app/src/main/java/com/prog20082/habitual/CreateHabitActivity.java
+++ app/src/main/java/com/prog20082/habitual/CreateHabitActivity.java
@@ -4,10 +4,15 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class CreateHabitActivity extends AppCompatActivity {
 
+  EditText edtHabitName;
+  EditText edtGoal;
+  Spinner spnGoalUnits;
 
 
     @Override
@@ -24,5 +29,11 @@ public class CreateHabitActivity extends AppCompatActivity {
     }
 
     public void submitHabit(View view) {
+
+      //Getting all the widgets from activity_create_habit.xml
+      edtHabitName = (EditText) findViewById(R.id.edtHabitName);
+
+
+
     }
 }
