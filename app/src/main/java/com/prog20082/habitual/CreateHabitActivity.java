7a9c0696ffcf379d95f4398ebe075cd11a919335
cottageGrove <rafae.pooba@gmail.com>
2016-11-24 17:19:45 -0500

    MainActivity goes to CreateHabitActivity and added getIntent on the OnCreate Method

--- app/src/main/java/com/prog20082/habitual/CreateHabitActivity.java
+++ app/src/main/java/com/prog20082/habitual/CreateHabitActivity.java
@@ -1,15 +1,26 @@
 package com.prog20082.habitual;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class CreateHabitActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_habit);
+
+      //Receive intent from MainActivity
+      Intent intent = getIntent();
+
+      //Receive a toast message
+      Toast.makeText(getApplicationContext(), "You're on the second page dorks", Toast.LENGTH_LONG).show();
+
     }
 
     public void submitHabit(View view) {
