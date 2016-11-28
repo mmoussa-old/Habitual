78cd80f02142cde2104a6bc30c9e952a64b28f22
DeveloperPrabh <prabhsimran007@gmail.com>
2016-11-28 13:16:34 -0500

    Added a new checkbox for users who want to get notified Notification is working when submitActivty is clicked

--- app/src/main/java/com/prog20082/habitual/CreateHabitActivity.java
+++ app/src/main/java/com/prog20082/habitual/CreateHabitActivity.java
@@ -1,5 +1,8 @@
 package com.prog20082.habitual;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,13 +18,14 @@ import android.widget.AdapterView.OnItemClickListener;
 
 public class CreateHabitActivity extends AppCompatActivity implements OnItemSelectedListener {
 
-  EditText edtHabitName;
-  EditText edtGoal;
-  Spinner spnGoalUnits;
-  CheckBox chkBadHabit;
-  CheckBox chkGoodHabit;
+    EditText edtHabitName;
+    EditText edtGoal;
+    Spinner spnGoalUnits;
+    CheckBox chkBadHabit;
+    CheckBox chkGoodHabit;
+    CheckBox chkNotify;
 
-  private static final int MAIN_ACTIVITY = 1;
+    private static final int MAIN_ACTIVITY = 1;
 
 
     @Override
@@ -29,55 +33,84 @@ public class CreateHabitActivity extends AppCompatActivity implements OnItemSele
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_habit);
 
-      //Receive intent from MainActivity
-      Intent intent = getIntent();
-      //Receive a toast message
-      Toast.makeText(getApplicationContext(), "You're on the second page dorks", Toast.LENGTH_LONG).show();
+        //Receive intent from MainActivity
+        Intent intent = getIntent();
+        //Receive a toast message
+        Toast.makeText(getApplicationContext(), "You're on the second page dorks", Toast.LENGTH_LONG).show();
 
-      //Getting all the widgets from activity_create_habit.xml
-      edtHabitName = (EditText) findViewById(R.id.edtHabitName);
-      edtGoal = (EditText) findViewById(R.id.edtGoal);
-      chkBadHabit= (CheckBox) findViewById(R.id.chkBadHabit);
-      chkGoodHabit = (CheckBox) findViewById(R.id.chkGoodHabit);
+        //Getting all the widgets from activity_create_habit.xml
+        edtHabitName = (EditText) findViewById(R.id.edtHabitName);
+        edtGoal = (EditText) findViewById(R.id.edtGoal);
+        chkBadHabit = (CheckBox) findViewById(R.id.chkBadHabit);
+        chkGoodHabit = (CheckBox) findViewById(R.id.chkGoodHabit);
 
-      spnGoalUnits = (Spinner) findViewById(R.id.spnGoalUnits);
 
-      //Create an ArrayAdapter using the string array and default spinner layout
-      ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-        R.array.goals_array, android.R.layout.simple_spinner_item);
-      //Choose what the list looks like when its selected
-      adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-      //apply adapter to spinner
-      spnGoalUnits.setAdapter(adapter);
+        spnGoalUnits = (Spinner) findViewById(R.id.spnGoalUnits);
 
-      spnGoalUnits.setOnItemSelectedListener(this);
+        //Added check for notifications
+        chkNotify = (CheckBox) findViewById(R.id.chkNotify);
+        //Create an ArrayAdapter using the string array and default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.goals_array, android.R.layout.simple_spinner_item);
+        //Choose what the list looks like when its selected
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //apply adapter to spinner
+        spnGoalUnits.setAdapter(adapter);
+
+        spnGoalUnits.setOnItemSelectedListener(this);
 
 
     }
 
+    //notification ID
+    public static int MY_NOTIFICATION = 1;
+
     public void submitHabit(View view) {
 
-      String habitName = edtHabitName.getText().toString();
-      String goal = edtGoal.getText().toString();
+        String habitName = edtHabitName.getText().toString();
+        String goal = edtGoal.getText().toString();
 
-      //Have to work on spnGoalUnits forgot that mo; onSelected junk
+        //Have to work on spnGoalUnits forgot that mo; onSelected junk
 
-      //Send a new parcelable habit for testing purposes to see if it goes to next screen
-      Habit parcelableHabit = new Habit(habitName, true, 3, goal);
-      Intent intent = new Intent(this, CreateHabitActivity.class);
-      intent.putExtra("habit", parcelableHabit);
-      setResult(RESULT_OK, intent);
+        //Send a new parcelable habit for testing purposes to see if it goes to next screen
+        Habit parcelableHabit = new Habit(habitName, true, 3, goal);
+        Intent intent = new Intent(this, CreateHabitActivity.class);
+        intent.putExtra("habit", parcelableHabit);
+        setResult(RESULT_OK, intent);
+
+        //Create a notification if the notification is checked.
+
+        if (chkNotify.isChecked()) {
+
+            NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            //When the pending intent is created, go back to mainActivity
+            Intent intentMain =  new Intent(this, MainActivity.class);
+            PendingIntent pi = PendingIntent.getActivity(this, 0, intentMain,0);
+            Notification notification = new Notification.Builder(this)
+                    .setWhen(1000)
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setTicker("Habit")
+                    .setContentTitle("Did you engage in " + habitName)
+                    .setContentIntent(pi)
+                    .setAutoCancel(true)
+                    .build();
+
+            nm.notify(MY_NOTIFICATION, notification);
+        }
+
 
     }
 
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View v, int pos, long id) {
-      parent.getItemAtPosition(pos);
+        parent.getItemAtPosition(pos);
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
 
     }
+
+
 }
