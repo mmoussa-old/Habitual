e468dc2e43c21397288aba71788e4ce4f3b32029
mmous <moussamo@sheridancollege.ca>
2016-12-05 03:20:21 -0500

    HabitDetailsActivity

---
+++ app/src/main/java/com/prog20082/habitual/HabitDetailsActivity.java
@@ -0,0 +1,13 @@
+package com.prog20082.habitual;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HabitDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_habit_details);
+    }
+}
